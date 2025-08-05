b860231891fe098039bd81f55537e6fc74a31fb8
Hansika-Prabodini <hansika@turintech.ai>
2025-08-05 18:31:15 +0530

    feat: update dataset handling in mosaic augmentation

--- computer_vision/mosaic_augmentation.py
+++ computer_vision/mosaic_augmentation.py
@@ -110,40 +110,51 @@ def update_image_and_anno(
     divid_point_x = int(scale_x * output_size[1])
     divid_point_y = int(scale_y * output_size[0])
 
-    new_anno = []
+    all_new_annos_np_list = []
     path_list = []
     for i, index in enumerate(idxs):
         path = all_img_list[index]
         path_list.append(path)
         img_annos = all_annos[index]
         img = cv2.imread(path)
+
+        if not img_annos:  # Handle cases where img_annos might be empty
+            if i == 0:  # top-left
+                img = cv2.resize(img, (divid_point_x, divid_point_y))
+                output_img[:divid_point_y, :divid_point_x, :] = img
+            elif i == 1:  # top-right
+                img = cv2.resize(img, (output_size[1] - divid_point_x, divid_point_y))
+                output_img[:divid_point_y, divid_point_x : output_size[1], :] = img
+            elif i == 2:  # bottom-left
+                img = cv2.resize(img, (divid_point_x, output_size[0] - divid_point_y))
+                output_img[divid_point_y : output_size[0], :divid_point_x, :] = img
+            else:  # bottom-right
+                img = cv2.resize(
+                    img, (output_size[1] - divid_point_x, output_size[0] - divid_point_y)
+                )
+                output_img[
+                    divid_point_y : output_size[0], divid_point_x : output_size[1], :
+                ] = img
+            continue # Skip to the next iteration if no annotations
+
+        # Convert to numpy array, separating class_ids and coords
+        class_ids = np.array([bbox[0] for bbox in img_annos]).reshape(-1, 1)
+        coords = np.array([bbox[1:] for bbox in img_annos], dtype=np.float32)
+
         if i == 0:  # top-left
             img = cv2.resize(img, (divid_point_x, divid_point_y))
             output_img[:divid_point_y, :divid_point_x, :] = img
-            for bbox in img_annos:
-                xmin = bbox[1] * scale_x
-                ymin = bbox[2] * scale_y
-                xmax = bbox[3] * scale_x
-                ymax = bbox[4] * scale_y
-                new_anno.append([bbox[0], xmin, ymin, xmax, ymax])
+            transformed_coords = coords * np.array([scale_x, scale_y, scale_x, scale_y])
         elif i == 1:  # top-right
             img = cv2.resize(img, (output_size[1] - divid_point_x, divid_point_y))
             output_img[:divid_point_y, divid_point_x : output_size[1], :] = img
-            for bbox in img_annos:
-                xmin = scale_x + bbox[1] * (1 - scale_x)
-                ymin = bbox[2] * scale_y
-                xmax = scale_x + bbox[3] * (1 - scale_x)
-                ymax = bbox[4] * scale_y
-                new_anno.append([bbox[0], xmin, ymin, xmax, ymax])
+            transformed_coords = coords * np.array([(1 - scale_x), scale_y, (1 - scale_x), scale_y])
+            transformed_coords[:, [0, 2]] = scale_x + transformed_coords[:, [0, 2]]
         elif i == 2:  # bottom-left
             img = cv2.resize(img, (divid_point_x, output_size[0] - divid_point_y))
             output_img[divid_point_y : output_size[0], :divid_point_x, :] = img
-            for bbox in img_annos:
-                xmin = bbox[1] * scale_x
-                ymin = scale_y + bbox[2] * (1 - scale_y)
-                xmax = bbox[3] * scale_x
-                ymax = scale_y + bbox[4] * (1 - scale_y)
-                new_anno.append([bbox[0], xmin, ymin, xmax, ymax])
+            transformed_coords = coords * np.array([scale_x, (1 - scale_y), scale_x, (1 - scale_y)])
+            transformed_coords[:, [1, 3]] = scale_y + transformed_coords[:, [1, 3]]
         else:  # bottom-right
             img = cv2.resize(
                 img, (output_size[1] - divid_point_x, output_size[0] - divid_point_y)
@@ -151,20 +162,24 @@ def update_image_and_anno(
             output_img[
                 divid_point_y : output_size[0], divid_point_x : output_size[1], :
             ] = img
-            for bbox in img_annos:
-                xmin = scale_x + bbox[1] * (1 - scale_x)
-                ymin = scale_y + bbox[2] * (1 - scale_y)
-                xmax = scale_x + bbox[3] * (1 - scale_x)
-                ymax = scale_y + bbox[4] * (1 - scale_y)
-                new_anno.append([bbox[0], xmin, ymin, xmax, ymax])
+            transformed_coords = coords * np.array([(1 - scale_x), (1 - scale_y), (1 - scale_x), (1 - scale_y)])
+            transformed_coords[:, [0, 2]] = scale_x + transformed_coords[:, [0, 2]]
+            transformed_coords[:, [1, 3]] = scale_y + transformed_coords[:, [1, 3]]
+
+        combined_anno_np = np.hstack((class_ids, transformed_coords))
+        all_new_annos_np_list.append(combined_anno_np)
+
+    # Concatenate all annotations
+    new_anno_np = np.vstack(all_new_annos_np_list) if all_new_annos_np_list else np.empty((0, 5))
 
     # Remove bounding box small than scale of filter
     if filter_scale > 0:
-        new_anno = [
-            anno
-            for anno in new_anno
-            if filter_scale < (anno[3] - anno[1]) and filter_scale < (anno[4] - anno[2])
-        ]
+        widths = new_anno_np[:, 3] - new_anno_np[:, 1]
+        heights = new_anno_np[:, 4] - new_anno_np[:, 2]
+        mask = (widths > filter_scale) & (heights > filter_scale)
+        new_anno_np = new_anno_np[mask]
+
+    new_anno = new_anno_np.tolist()
 
     return output_img, new_anno, path_list[0]
 
